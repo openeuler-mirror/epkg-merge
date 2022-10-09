57c6cf8f8f789acf52b33c75db81b06d45b0dbb2
wu_zhende <wuzhende666@163.com>
2022-10-09 16:03:08 +0800

    add expand and transform

--- src/core/evaluator/expand.py
+++ src/core/evaluator/expand.py
@@ -1,5 +1,29 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-def expand_macro(val):
-    return val
+import re
+
+from src.core.config_space import config_space
+
+
+def expand_macro(str_macro):
+    str_macro += " "
+    patterns = [r'(%%%?{?(.+?)[}" "\s])', r'(dd?\.(.+?)[" "\s])']
+    macro_keys = {}
+    for pattern in patterns:
+        for match in re.findall(pattern, str_macro):
+            macro_keys[match[0].strip()] = match[1]
+    str_macro = str_macro[0:-1]
+    sub_values = {}
+    for k, v in macro_keys.items():
+        sub_values[k] = config_space.get_key(v)
+    return substitute(str_macro, sub_values)
+
+
+def substitute(str_macro, sub_values):
+    # 先替换长度大的字符串
+    # 防止出现%%abcd被%%a先错误替换的情况
+    sorted_sub_values = sorted(sub_values.items(), key=lambda x: len(x[0]), reverse=True)
+    for sub_value in sorted_sub_values:
+        str_macro = str_macro.replace(sub_value[0], sub_value[1])
+    return str_macro
--- src/core/evaluator/transform.py
+++ src/core/evaluator/transform.py
@@ -1,3 +1,50 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2022 Huawei Technologies Co., Ltd. All rights reserved.
+import os
 
+
+def transform_include_phase(file):
+    if not os.path.exists(file):
+        return {}
+    with open(file, "r") as f:
+        content = f.readlines()
+        return parse_shell_file(content)
+
+
+def parse_shell_file(content):
+    functions = {}
+    function_name = ""
+    function_content = ""
+    for line in content:
+        _line = line.rstrip()
+        if function_name:
+            if _line == "{":
+                continue
+            if _line == "}":
+                functions[function_name] = function_content
+                function_name, function_content = "", ""
+                continue
+            function_content += line
+        else:
+            function_name = get_shell_function_name(line)
+    return functions
+
+
+def get_shell_function_name(line):
+    if line.startswith("function"):
+        name = line.strip().split()[1]
+        return parse_function_name(name)
+    if len(line.split()) > 2:
+        return ""
+    if "()" not in line:
+        return ""
+    name = line.split()[0]
+    return parse_function_name(name)
+
+
+def parse_function_name(name):
+    if name.endswith("{"):
+        name = name[0:-1]
+    if name.endswith("()"):
+        name = name[0:-2]
+    return name
