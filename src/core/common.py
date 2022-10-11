df6fd9b8710bf650078efbc2b59445a373af289f
Duan Pengjie <pengjieduan@gmail.com>
2022-10-11 14:38:43 +0800

    change set_key to add_key

--- src/core/common.py
+++ src/core/common.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-def is_pycode(val:str):
+def is_pycode(val: str):
     val = val.strip()
     if not val.startswith("{{"):
         return False
--- src/core/loader/yaml_loader.py
+++ src/core/loader/yaml_loader.py
@@ -29,7 +29,7 @@ class YamlLoader:
         configs = yaml.safe_load(open(self._fspath, encoding="utf-8"))
         result = expand_yaml(configs, self._cspath)
         for k, v in result.items():
-            config_space.set_key(k, v, self._fspath, None)
+            config_space.add_key(k, v, self._fspath, None)
             if ":" not in k:
                 config_space.setdefault(f"{self._cspath}:loadedKeys", set()).add(k)
 
@@ -46,7 +46,7 @@ class YamlLoader:
                     transform_result = getattr(transform, func)(os.path.join(os.path.dirname(self._fspath), f))
                     result = expand_yaml(transform_result, self._cspath)
                     for k, v in result.items():
-                        config_space.set_key(k, v, self._fspath, None)
+                        config_space.add_key(k, v, self._fspath, None)
                         if ":" not in k:
                             config_space.setdefault(f"{self._cspath}:loadedKeys", set()).add(k)
 
