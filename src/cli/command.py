057d94e6511fd61421807e0d61c5d452898295d2
wu_zhende <wuzhende666@163.com>
2022-10-12 16:57:29 +0800

    cli: support for multiple packages

--- src/cli/command.py
+++ src/cli/command.py
@@ -1,39 +1,59 @@
 # SPDX-License-Identifier: MulanPSL-2.0+
 # Copyright (c) 2022 Huawei Technologies Co., Ltd. All rights reserved.
 
+import os
 import yaml
 import argparse
 
+from src.core.py_interpreter.interpreter import startup
+
 
 def handle_load(file):
-    from src.core.loader.yaml_loader import YamlLoader
-    YamlLoader.load(file)
+    from src.core.loader.layer_loader import LayerLoader
+    LayerLoader(file).load()
 
 
 def handle_package(package):
     from src.core.config_space import config_space
-    return config_space.get_package(package)
+    return config_space.get_package_format_json(package)
 
 
-def handle_output(content, output):
+def handle_output(package_name, content, output):
     if not content:
         return
     if not output:
-        output = "./package.yaml"
-    f = open(output, 'w')
-    yaml.dump(content, f)
+        output = "./" + package_name
+    # $output/$package_name.yaml
+    if not os.path.exists(output):
+        os.makedirs(output)
+    file = output + "/" + package_name + ".yaml"
+    with open(file, "w") as f:
+        yaml.SafeDumper.org_represent_str = yaml.SafeDumper.represent_str
+
+        def repr_str(dumper, data):
+            if '\n' in data:
+                return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='|')
+            return dumper.org_represent_str(data)
+
+        yaml.add_representer(str, repr_str, Dumper=yaml.SafeDumper)
+        yaml.safe_dump(content, f, allow_unicode='uft-8')
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config_file", help="the configuration file to be parsed")
-    parser.add_argument("-p", "--package", help="the parsed package name")
-    parser.add_argument("-o", "--output", help="which file the output is redirected to")
+    parser.add_argument("-p", "--packages", help="the parsed packages， use -p 'A B' to specify multiple packages")
+    parser.add_argument("-o", "--output", help="which dir the output is redirected to")
     args = vars(parser.parse_args())
     if args["config_file"]:
         handle_load(args["config_file"])
-    package_info = handle_package(args["package"])
-    handle_output(package_info, args["output"])
+        startup(args["config_file"])
+    packages = []
+    if args["packages"]:
+        packages = args["packages"].split()
+    for package in packages:
+        package_info = handle_package(package)
+        handle_output(package, package_info, args["output"])
 
 
 if __name__ == '__main__':
