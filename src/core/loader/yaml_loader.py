a2320c5294ac10fbf0591f7e40f17f155c249e64
niexinquan <3290937695@qq.com>
2023-05-22 13:04:00 +0800

    支持宏参数字段

--- src/core/loader/yaml_loader.py
+++ src/core/loader/yaml_loader.py
@@ -90,8 +90,8 @@ class YamlLoader:
                     result = expand_yaml(transform_result, self._cspath)
                     for k, v in result.items():
                         actual_keys = config_space.add_key(k, v, self._fspath)
-                        if ":" in k:
-                            continue
+                        # if ":" in k:
+                        #     continue
                         for actual_key in actual_keys:
                             config_space.setdefault(f"{self._cspath}:loadedKeys", set()).add(actual_key)
 
