0402c0d08e5b7a323f3eb2ceb340b854e871e661
Duan Pengjie <pengjieduan@gmail.com>
2023-07-31 23:56:17 +0800

    优化when条件策略，增加remove属性

--- src/core/loader/layer_loader.py
+++ src/core/loader/layer_loader.py
@@ -90,7 +90,7 @@ class _LayerConfigLoader:
                     break
 
             if not pkg_config:
-                log.error(f"layer '{self._layer}' lacks of {pkg}.yaml")
+                log.error(f"layer '{self._layer}' lacks of {pkg}/package.yaml")
                 continue
 
             _ElementConfigLoader(pkg, pkg_config, index_config).load()
