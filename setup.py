98cabeed8d686298b1818c61a9d138215802449c
Duan Pengjie <pengjieduan@gmail.com>
2023-08-15 11:42:47 +0800

    打包v0.1.0版本

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 # python3 setup.py bdist_wheel
 setup(
     name="merge-configs",
-    version="0.0.6",
+    version="0.1.0",
     packages=find_packages(),
     description="...",
     license="MulanPSL-2.0+",
